682fe7db5ace812ece81e784ff26c7c4bf5cf8bd
amine-sakka <aminesakka973@gmail.com>
2020-07-02 12:54:32 +0100

    tests

--- spring-boot-app/src/test/java/com/api/BlogApi1ApplicationTests.java
+++ spring-boot-app/src/test/java/com/api/BlogApi1ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.thegeekyasian;
+package com.api;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,10 +7,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DockerSpringBootMysqlApplicationTests {
+public class BlogApi1ApplicationTests {
 
-    @Test
-    public void contextLoads() {
-    }
+	@Test
+	public void contextLoads() {
+	}
 
 }
