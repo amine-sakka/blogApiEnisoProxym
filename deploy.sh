ef3f09044d5f3ab757e02fcb4658c26d010f8c35
amine-sakka <aminesakka973@gmail.com>
2020-07-02 12:36:19 +0100

    jul 2,12:36:15

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,12 @@
 
 function stop() {
 	echo "Shutting down docker."
-	sudo docker-compose down
+	docker-compose down
 	echo "Stopped bash opeation."
 	exit 2
 }
 
 trap "stop" 2
 
-cd ./spring-boot-app &&
- sudo mvn clean package -DskipTests &&
- cd .. &&
- sudo docker-compose build &&
- sudo docker-compose up
+  docker-compose build &&
+  docker-compose up
